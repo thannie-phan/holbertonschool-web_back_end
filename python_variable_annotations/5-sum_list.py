02d5b6c5f2eaf70d96a8491cd802b18fa1f174b4
thannie-phan <thannie-phan@gmail.com>
2025-11-17 15:37:36 +1100

    task2

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,6 +3,5 @@
 from typing import List
 
 def sum_list(input_list: List[float]) -> float:
-    """every element inside list should be a float. return sum of elements of the list as float"""
-    
+    """every element inside list should be a float. return sum of list loat"""
     return sum(input_list)
