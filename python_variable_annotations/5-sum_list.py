b316d950ee0283d1a244f0ff786a8ccfa042ea9c
thannie-phan <thannie-phan@gmail.com>
2025-11-17 15:35:43 +1100

    task2

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Define variables"""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """every element inside list should be a float. return sum of elements of the list as float"""
+    
+    return sum(input_list)
