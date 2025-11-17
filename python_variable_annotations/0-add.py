e13832a236107c5fe39eab44bee610a7053be98b
thannie-phan <thannie-phan@gmail.com>
2025-11-17 14:22:23 +1100

    add folder

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic annotations - add"""
+
+
+def add(a: float, b: float) -> float:
+    """a, b should be float and -> aka return should also be float"""
+
+    return a + b
