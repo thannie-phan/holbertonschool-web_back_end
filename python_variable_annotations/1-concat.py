b3826d1ace74fbbc9f12c59761ee0ee5b5520fe5
thannie-phan <thannie-phan@gmail.com>
2025-11-17 14:25:42 +1100

    task1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic annotations - add"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """str1 and 2 should be str and -> aka return should also be str"""
+
+    return str1 + str2
