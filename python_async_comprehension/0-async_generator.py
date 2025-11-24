85df6d1b932ceeeb7b45294be9e3a7bc23f4223a
thannie-phan <thannie-phan@gmail.com>
2025-11-24 14:50:19 +1100

    task0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" async generator"""
+import asyncio
+from typing import AsyncGenerator
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, None, None]:
+    """ Async Generator """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
