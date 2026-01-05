b31b87228508d7ad011f2a5f4fa6fe56e1cc190e
thannie-phan <thannie-phan@gmail.com>
2026-01-05 13:53:35 +1100

    task9

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,14 +1,18 @@
-function getCurrentYear() {
-  const date = new Date();
-  return date.getFullYear();
-}
+import getBudgetObject from './7-getBudgetObject.js';
 
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {
-    [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita,
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    ...income,
+
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
   };
 
-  return budget;
+  return fullBudget;
 }
