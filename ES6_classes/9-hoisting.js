f84e39a5aee1a86ea96a3adaa2b96df0f02a6ff8
thannie-phan <thannie-phan@gmail.com>
2026-01-06 13:26:53 +1100

    task9

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -15,22 +15,22 @@ export class HolbertonClass {
 
 
 export class StudentHolberton {
-    constructor(firstName, lastName, HolbertonClass) {
+    constructor(firstName, lastName, holbertonClass) {
         this._firstName = firstName;
         this._lastName = lastName;
-        this._HolbertonClass = HolbertonClass;
+        this._holbertonClass = holbertonClass;
     }
 
     get fullName() {
         return `${this._firstName} ${this._lastName}`;
     }
 
-    get HolbertonClass() {
-        return this._HolbertonClass;
+    get holbertonClass() {
+        return this._holbertonClass;
     }
 
     get fullStudentDescription() {
-        return `${this._firstName} ${this._lastName} - ${this._HolbertonClass.year} - ${this._HolbertonClass.location}`;
+        return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
     }
 }
 
