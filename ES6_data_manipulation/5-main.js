605e78dd7e58dd1e77646a52dc61fcb933434d28
thannie-phan <thannie-phan@gmail.com>
2026-01-07 17:57:57 +1100

    task5

--- ES6_data_manipulation/5-main.js
+++ ES6_data_manipulation/5-main.js
@@ -1,14 +1,3 @@
-import Building from './5-building.js';
+import createInt8TypedArray from "./5-typed_arrays.js";
 
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building { }
-
-try {
-    new TestBuilding(200)
-}
-catch (err) {
-    console.log(err);
-}
-
+console.log(createInt8TypedArray(10, 2, 89));
