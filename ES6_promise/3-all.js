35b2cdc5cd227e48d994acf08d121f4202d293ec
thannie-phan <thannie-phan@gmail.com>
2026-01-09 11:53:04 +1100

    task3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+    Promise.all([uploadPhoto(), createUser()])
+        .then((results) => { // if promise is all resolved
+            const photo = results[0];
+            const user = results[1];
+
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => { // if not
+            console.log('Signup system offline');
+        });
+}
--- ES6_promise/3-main.js
+++ ES6_promise/3-main.js
@@ -1,6 +1,3 @@
-import getListStudents from "./0-get_list_students.js";
-import getStudentIdsSum from "./3-get_ids_sum.js";
+import handleProfileSignup from "./3-all";
 
-const students = getListStudents();
-const value = getStudentIdsSum(students);
-console.log(value);
+handleProfileSignup();
