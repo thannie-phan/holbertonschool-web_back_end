11ceca97bf5b067cb2004660b74d2aa4df23180e
thannie-phan <thannie-phan@gmail.com>
2026-01-09 11:58:01 +1100

    task4

--- ES6_promise/4-main.js
+++ ES6_promise/4-main.js
@@ -1,6 +1,3 @@
-import getListStudents from "./0-get_list_students.js";
-import updateStudentGradeByCity from "./4-update_grade_by_city.js";
+import signUpUser from "./4-user-promise";
 
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
-
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
+console.log(signUpUser("Bob", "Dylan"));
