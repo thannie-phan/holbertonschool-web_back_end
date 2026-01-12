69129b7c3e1a76c59d643d419ebff4716f0c7759
thannie-phan <thannie-phan@gmail.com>
2026-01-12 22:24:48 +1100

    task6

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -4,14 +4,11 @@ const express = require('express');
 
 const app = express();
 
-// express has built in app.get to help handle header + auto end response handling. 
+// express has built in app.get to help handle header + auto end response handling.
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(1245);
 
 module.exports = app;
-
-
-
