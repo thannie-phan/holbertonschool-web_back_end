d933e892d30f577149ac0d832419791a7520405f
thannie-phan <thannie-phan@gmail.com>
2026-01-12 22:20:14 +1100

    task6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,17 @@
+// use express instead of http
+
+const express = require('express');
+
+const app = express();
+
+// express has built in app.get to help handle header + auto end response handling. 
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
+
+
+
